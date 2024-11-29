9e97684b1f964c12e81a69541ffac9168edbf349
Lucas Pereira <lucas.carvalho@copel.com>
2024-11-29 18:42:41 -0300

    fix: code review

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -2,16 +2,6 @@ import { StyleSheet } from "react-native";
 
 const baseStyles = StyleSheet.create({
     input: {
-        // flex: 1,
-        // height: 52,
-        // backgroundColor: "#262626",
-        // borderWidth: 1,
-        // borderColor: "#0D0D0D",
-        // borderRadius: 6,
-        // color: "#F2F2F2",
-        // padding: 16,
-        // fontSize: 16,
-
         height: 40,
         borderColor: "#ccc",
         borderWidth: 1,
@@ -20,7 +10,6 @@ const baseStyles = StyleSheet.create({
         color: "#F2F2F2",
     },
     button: {
-        // padding: 16,
         marginTop: 14,
         height: 52,
         borderRadius: 6,
